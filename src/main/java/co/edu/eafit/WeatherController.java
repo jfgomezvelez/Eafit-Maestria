b1194a64a836b80992ef4800998e89bdcc61cb34
Juan Felipe Gomez Velez <juan.gomez@sofka.com.co>
2023-04-09 19:13:45 -0500

    se guarda el processs type como este configurado en el application.yaml

--- src/main/java/co/edu/eafit/WeatherController.java
+++ src/main/java/co/edu/eafit/WeatherController.java
@@ -30,7 +30,7 @@ public class WeatherController {
         ProcessData process = new ProcessData(
                 LocalTime.now(),
                 processType,
-                FeatureType.COMMANDMESSAGE.toString().concat("-").concat(processType)
+                processType
         );
 
         statisticRepository.save(process);
